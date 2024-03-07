b8dc3e36c774cc49283cf286a2b4c4b2c65c9a54
julianosoriosalebe <julianosoriosalebe@gmail.com>
2024-03-06 21:19:58 -0500

    Added SCRIPTABLE OBJECT architecture
    Added architecture for better structure

---
+++ Assets/Scripts/Scriptables/Game/GameStateSO.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "GameState", menuName = "Scriptable Object/GameState")]
+
+public class GameStateSO : ScriptableObject
+{
+    public string GameState;
+}
